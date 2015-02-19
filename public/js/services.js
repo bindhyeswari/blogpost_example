246c84c743ce9ff570431476f748e5893258a76a
pvtech <tech@golivelabs.io>
2015-02-18 17:33:03 -0800

    info

---
+++ public/js/services.js
@@ -0,0 +1,6 @@
+angular.module('blogapp.services', ['ngResource']).factory('Blog', function ($resource){
+
+    // todo: Write the service without $resource ..
+
+    return $resource('/posts/:id');
+});
---
+++ routes/blogs.js
@@ -0,0 +1,35 @@
+var router = require('express').Router();
+var mongoose = require('mongoose');
+
+var Blog = mongoose.model('Blog', {
+    title: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    content: String
+});
+
+router.get('/', function (req, res) {
+    Blog.find(function (err, result) {
+        if (err) res.status(500).json({ message: 'Something broke ... ' });
+        else res.status(200).json(result);
+    });
+});
+
+router.get('/:id', function (req, res) {
+    Blog.findById(req.params.id, function (err, result) {
+        console.log(result);
+        if (err) res.status(500).json({ message: 'Something broke ... ' });
+        else res.status(200).json(result);
+    });
+});
+
+router.post('/', function (req, res) {
+    (new Blog(req.body)).save(function (err, result) {
+        if (err) res.status(500).json({message: 'Something broke ... '});
+        else res.status(200).json(result);
+    });
+});
+
+module.exports = router;
